fc180660c8f15b5e9ae7e2ede834ad8ae4ee134d
complex-mixture <765465865@qq.com>
2017-12-12 15:28:45 +0800

    GUI part over
    no

---
+++ Show/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Show
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PictureBoxFCFS = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSSTF = new System.Windows.Forms.PictureBox();
+            this.pictureBoxSCAN = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLOOK = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxFCFS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSSTF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSCAN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLOOK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBoxFCFS
+            // 
+            this.PictureBoxFCFS.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PictureBoxFCFS.BackgroundImage")));
+            this.PictureBoxFCFS.Location = new System.Drawing.Point(0, 0);
+            this.PictureBoxFCFS.Name = "PictureBoxFCFS";
+            this.PictureBoxFCFS.Size = new System.Drawing.Size(675, 368);
+            this.PictureBoxFCFS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBoxFCFS.TabIndex = 0;
+            this.PictureBoxFCFS.TabStop = false;
+            // 
+            // pictureBoxSSTF
+            // 
+            this.pictureBoxSSTF.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxSSTF.BackgroundImage")));
+            this.pictureBoxSSTF.Location = new System.Drawing.Point(675, 0);
+            this.pictureBoxSSTF.Name = "pictureBoxSSTF";
+            this.pictureBoxSSTF.Size = new System.Drawing.Size(675, 368);
+            this.pictureBoxSSTF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSSTF.TabIndex = 1;
+            this.pictureBoxSSTF.TabStop = false;
+            // 
+            // pictureBoxSCAN
+            // 
+            this.pictureBoxSCAN.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxSCAN.BackgroundImage")));
+            this.pictureBoxSCAN.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSCAN.Image")));
+            this.pictureBoxSCAN.Location = new System.Drawing.Point(675, 369);
+            this.pictureBoxSCAN.Name = "pictureBoxSCAN";
+            this.pictureBoxSCAN.Size = new System.Drawing.Size(675, 368);
+            this.pictureBoxSCAN.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxSCAN.TabIndex = 3;
+            this.pictureBoxSCAN.TabStop = false;
+            // 
+            // pictureBoxLOOK
+            // 
+            this.pictureBoxLOOK.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxLOOK.BackgroundImage")));
+            this.pictureBoxLOOK.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxLOOK.Image")));
+            this.pictureBoxLOOK.Location = new System.Drawing.Point(0, 369);
+            this.pictureBoxLOOK.Name = "pictureBoxLOOK";
+            this.pictureBoxLOOK.Size = new System.Drawing.Size(675, 368);
+            this.pictureBoxLOOK.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxLOOK.TabIndex = 2;
+            this.pictureBoxLOOK.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FCFS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(688, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 27);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SSTF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(12, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 27);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "LOOK";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(688, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SCAN";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 17;
+            this.timer.Tick += new System.EventHandler(this.Update);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxSCAN);
+            this.Controls.Add(this.pictureBoxLOOK);
+            this.Controls.Add(this.pictureBoxSSTF);
+            this.Controls.Add(this.PictureBoxFCFS);
+            this.MaximumSize = new System.Drawing.Size(1366, 768);
+            this.MinimumSize = new System.Drawing.Size(1366, 768);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxFCFS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSSTF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSCAN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLOOK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBoxFCFS;
+        private System.Windows.Forms.PictureBox pictureBoxSSTF;
+        private System.Windows.Forms.PictureBox pictureBoxSCAN;
+        private System.Windows.Forms.PictureBox pictureBoxLOOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
